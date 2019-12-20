e39182a7ce65cfef2b7e2ccbd3c74592d7a07390
metelskiwiktor <metelski.wiktor@gmail.com>
2019-12-20 23:50:13 +0100

    Initial commit

---
+++ src/main/java/pl/wiktor/shop/shopadministration/ShopAdministrationApplication.java
@@ -0,0 +1,13 @@
+package pl.wiktor.shop.shopadministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAdministrationApplication.class, args);
+	}
+
+}
